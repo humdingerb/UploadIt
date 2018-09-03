95b446e7d5316b58ffcde3a48f83089208170916
Humdinger <humdingerb@gmail.com>
2018-09-03 14:30:22 +0200

    Don't accept folders for uploading
    Put a note into the clipboard that UploadIt only works with files,
    if the user tries to send a folder.
    
    Update catkeys.

--- main.cpp
+++ main.cpp
@@ -27,20 +27,39 @@ process_refs(entry_ref directoryRef, BMessage* msg, void*)
 {
 	BPath path;
 	entry_ref file_ref;
+
 	if (msg->FindRef("refs", &file_ref) == B_NO_ERROR) {
-		BEntry(&file_ref).GetPath(&path);
-		BString command(
-			"stat=$(curl -m 2 -s -I http://google.com | grep HTTP/1 | awk {'print $2'}) ; "
-			"if [ -z  \"$stat\" ] ; then "	// network up in general?
-				"clipboard -c \"%ERROR%\" ; "
-			"else "
-				"curl -F'file=@'\"%FILENAME%\" https://0x0.st | clipboard -i ; "
-			"fi ; "
-			"exit");
-		command.ReplaceAll("%ERROR%",
-			B_TRANSLATE("Online upload service not available"));
-		command.ReplaceAll("%FILENAME%", path.Path());
-		system(command.String());
+		BEntry entry(&file_ref);
+		if (entry.IsDirectory()) {
+			BString text(B_TRANSLATE(
+				"UploadIt only works on a single file, no folders"));
+			ssize_t textLen = text.Length();
+			BMessage* message = (BMessage *)NULL;
+
+			if (be_clipboard->Lock()) {
+				be_clipboard->Clear();
+				if ((message = be_clipboard->Data())) {
+					message->AddData("text/plain", B_MIME_TYPE, text.String(),
+						textLen);
+					be_clipboard->Commit();
+				}
+				be_clipboard->Unlock();
+			}
+		} else {
+			entry.GetPath(&path);
+			BString command(
+				"stat=$(curl -m 2 -s -I http://google.com | grep HTTP/1 | awk {'print $2'}) ; "
+				"if [ -z  \"$stat\" ] ; then "	// network up in general?
+					"clipboard -c \"%ERROR%\" ; "
+				"else "
+					"curl -F'file=@'\"%FILENAME%\" https://0x0.st | clipboard -i ; "
+				"fi ; "
+				"exit");
+			command.ReplaceAll("%ERROR%",
+				B_TRANSLATE("Online upload service not available"));
+			command.ReplaceAll("%FILENAME%", path.Path());
+			system(command.String());
+		}
 	}
 }
 
